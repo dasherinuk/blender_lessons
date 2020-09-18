7f461758f834f942a83c63c4e0c03d459a05717c
dasherinuk <70596595+dasherinuk@users.noreply.github.com>
2020-09-18 21:23:50 +0100

    Add files via upload

---
+++ appendExample.py
@@ -0,0 +1,7 @@
+array=[1,2,3,4,5,6]
+array.append(7)
+print(array)
+userArray=[]
+for index in range(0,10):
+    userArray.append(index)
+print(userArray)
---
+++ dz_home2.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame import *
+
+size_square = 50
+count_row = 5
+
+width=800
+height=600
+
+screen = pygame.display.set_mode((width,height))
+
+surface = Surface((size_square,size_square))
+surface.fill((0,0,255))
+
+while 1:
+    event = pygame.event.wait()
+    if event.type == pygame.QUIT:
+        break
+    screen.fill((255,0,0))
+
+    for ind in range(width//size_square//2):
+        screen.blit(surface,(size_square*2*ind,200))
+    for ind in range(3,height//size_square//2):
+        screen.blit(surface,(0,size_square*2*ind))
+    for ind in range(width//size_square//2):
+        screen.blit(surface,(size_square*2*ind, height -  size_square))
+    for ind in range(3,height//size_square//2):
+        screen.blit(surface,(width-size_square*2,size_square*2*ind))
+
+    pygame.display.update()
